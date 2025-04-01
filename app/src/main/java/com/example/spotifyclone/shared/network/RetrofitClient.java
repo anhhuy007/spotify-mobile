c9bd28cd0a7b264c0c01b324d1a73f952319a986
vominh-source <117811638+vominh-source@users.noreply.github.com>
2025-04-01 10:38:20 +0700

    aciton update RetrofitClient.java

--- app/src/main/java/com/example/spotifyclone/shared/network/RetrofitClient.java
+++ app/src/main/java/com/example/spotifyclone/shared/network/RetrofitClient.java
@@ -13,9 +13,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-//    private static final String BASE_URL="https://spotify-server-7bcw.onrender.com/";
+    private static final String BASE_URL="https://spotify-server-7bcw.onrender.com/";
 
-    private static final String BASE_URL="http://127.0.0.1:4000/";
 
     private static Retrofit retrofit = null;
 
